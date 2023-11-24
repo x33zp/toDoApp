8e0947fbd3b302b8f0c58dafabefafed3bfe27fb
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-24 10:14:42 +0100

    importing the database

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import {
     initializeApp
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+// Challenge: Import the 'getDatabase' from
+// "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {
+    getDatabase
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 
 const appSettings = {
     databaseURL: "https://playground-5f5ea-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
+const dataBase =
 
-console.log(app)
-
-let inputFieldEl = document.querySelector("#input-field")
+    let inputFieldEl = document.querySelector("#input-field")
 const addBtnEl = document.querySelector("#add-button")
 
 addBtnEl.addEventListener("click", () => {
