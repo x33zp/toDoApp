00ac8f7b19accd93b56633c06846d8680eb5ae33
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-24 10:11:33 +0100

    creating a varible app that connects our project with the project that contains the appSettings database

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import {
     initializeApp
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 
-// Challenge: Make sure that add is working by console logging add with two number arguments.
-console.log(add(2, 3))
-
 const appSettings = {
     databaseURL: "https://playground-5f5ea-default-rtdb.firebaseio.com/"
 }
+
+const app = initializeApp(appSettings)
+
+console.log(app)
 
 let inputFieldEl = document.querySelector("#input-field")
 const addBtnEl = document.querySelector("#add-button")
