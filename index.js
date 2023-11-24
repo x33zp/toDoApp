5089ddbf5cf9a3a6a0b2c5a8ac1f712168dc95a7
zubby peculiar <zubbypeculiar@gmail.com>
2023-11-24 21:52:16 +0100

    Append a new <li> with text content inputValue to the 'shopping-list' and clear input field value after item is added

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ import {
     push
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-
 const appSettings = {
-    databaseURL: "https://playground-5f5ea-default-rtdb.firebaseio.com/"
+    databaseURL: "https://tasklist-e141a-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
-const dataBase = getDatabase(app)
-const movieInDB = ref(dataBase, "movies")
+const database = getDatabase(app)
+const taskListInDB = ref(database, "taskList")
 
-let inputFieldEl = document.querySelector("#input-field")
-const addBtnEl = document.querySelector("#add-button")
+const inputFieldEl = document.querySelector("#input-field")
+const addButtonEl = document.querySelector("#add-button")
+const ulEl = document.querySelector("#shopping-list")
 
-addBtnEl.addEventListener("click", () => {
+addButtonEl.addEventListener("click", () => {
     let inputValue = inputFieldEl.value
 
-    push(movieInDB, inputValue)
-
-    console.log(inputValue)
+    push(taskListInDB, inputValue)
+    ulEl.innerHTML = `<li>${inputValue}</li>`
+    inputFieldEl.value = ""
 })
